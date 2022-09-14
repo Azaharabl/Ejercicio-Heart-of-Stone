f817d9b073e2690460ef5bb613b959a9d2f73c30
Azahara Blanco Rodriguez <blancaluna92@hotmail.com>
2022-09-15 00:23:42 +0200

    conseguido dolucionar el problema del sinbleton, pero no se si correctamente

--- src/main/kotlin/pojo/Matriz.kt
+++ src/main/kotlin/pojo/Matriz.kt
@@ -4,10 +4,41 @@ import interfaces.IMatrid
 
 //pongo object porque quiero que sea singleton
 object Matriz : IMatrid {
+    lateinit var matriz :ArrayList<ArrayList<Item>>
+
     init {
 
-        val tamaño:Int =
-        var  matriz : MutableList<MutableList<Item>>
+        //pedir el tamaño
+
+        val tamaño:Int? = pedirElTamaño()
+        var  matriz : ArrayList<ArrayList<Item>>
+
+        //inicializar matriz
+        iniciarMatriz(tamaño)
+
+    }
+
+    private fun iniciarMatriz(tamaño: Int?) {
+        ArrayList
+        for (i in tamaño until){
+            ArrayList
+        }
+    }
+
+    
+
+    ///// llevo dos horas, me falta media, voy por aqui
+
+    private fun pedirElTamaño() : Int? {
+       var tamanio : Int?
+       var esCorrecto : Boolean = false
+        do {
+            println("elige el tamaño del tablero entre 6 ,8 o 10")
+            tamanio = readLine()?.toIntOrNull()
+            if(tamanio ==6 || tamanio==8 || tamanio==10){ esCorrecto=true}
+        }while (!esCorrecto)
+
+        return tamanio
 
     }
 
